72599ccedc603e43f7ef1f9a1e752dab9866f6f7
AlexandruPopovici <alexandrupopoviciioan@gmail.com>
2017-08-31 09:50:43 +0300

    all

---
+++ Assets/src/character/Cog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cog : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+        yield return new WaitForSeconds(1);
+        GetComponent<Animator>().SetFloat("Speed", 1);
+        yield return new WaitForSeconds(2);
+        GetComponent<Animator>().SetFloat("Speed", 0);
+        //GetComponent<Animator>().SetTrigger("Go");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
